a61a5c2620a97b2bd3d5f26f995dc030fe593b67
fdoromo <fdoromo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-25 18:15:10 +0000

    tags 1.5
    git-svn-id: https://plugins.svn.wordpress.org/wp-mobile-edition/tags/1.5@673127 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ themes/mobile-edition-mTheme/inc-menu.php
@@ -0,0 +1,5 @@
+<div class="fdx_categories" style="display: inline !important">
+<ul>
+<?php wp_nav_menu( array( 'menu' => 'fdx-menu', 'link_before' => '<div class="togle">', 'link_after' => '</div>', 'items_wrap' => '%3$s', 'depth' => '1' ) ); ?>
+</ul>
+</div>
