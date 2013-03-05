fa15df3cb6929120ab98dc6d302a028964f6c6e8
fdoromo <fdoromo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-05 17:01:25 +0000

    tags 1.7
    git-svn-id: https://plugins.svn.wordpress.org/wp-mobile-edition/tags/1.7@676529 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ themes/mobile-edition-mTheme/404.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<div class="fdx_topheading">404 - <?php _e('Page not found', 'fdx-lang') ?></div>
+<div class="fdx_content">
+<div align="center" style="padding: 30px 0 30px 0; "><strong style="color:red; font-size: 30px">404</strong><br /><strong><?php _e('Page not found', 'fdx-lang') ?> </strong>
+</div>
+</div>
+<?php get_footer(); ?>
