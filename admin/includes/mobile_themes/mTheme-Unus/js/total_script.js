c5d05a1ba29a44f5abd45304748437865bbbd5b4
fdoromo <fdoromo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-19 13:15:37 +0000

    tag 2.2 js
    git-svn-id: https://plugins.svn.wordpress.org/wp-mobile-edition/trunk@935079 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/includes/mobile_themes/mTheme-Unus/js/total_script.js
+++ admin/includes/mobile_themes/mTheme-Unus/js/total_script.js
@@ -5,12 +5,17 @@ $(document).ready(function() {
         return false;
     });
 
+
 // menu
+     $('.fdx_categories2').hide();
+     $('.fdx_search').hide();
+     $('.fdx_contact').hide();
+     $(".sub-menu").hide();
+
 	 $('.fdx_catlink > a').click(function() {
      $('.fdx_categories2').slideToggle('slow');
      $('.fdx_search,.fdx_contact').slideUp();
-
-	});
+ 	});
 
 	$('.fdx_searchlink > a').click(function() {
 		$('.fdx_search').slideToggle('slow');
@@ -20,13 +25,11 @@ $(document).ready(function() {
 	$('.fdx_contactlink > a').click(function() {
 		$('.fdx_contact').slideToggle('slow');
         $('.fdx_categories2,.fdx_search').slideUp();
-
 	});
 
-
- //sun,enu
-	$('.menu-menu1-container').click(function(){
-	$("ul.sub-menu").slideToggle();
+ //sun,menu
+	$('.menu-item-has-children > a').click(function(){
+	$(".sub-menu").slideToggle('slow');
 	});
 
 
